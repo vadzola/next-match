160460e3fd040d9a57bb3914ed00a4e15115eddc
vadzola <vadzola@yahoo.com.hk>
2024-06-25 21:03:32 +0200

    prva verzija

---
+++ src/app/members/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MemberPage() {
+  return (
+    <div>
+    <h3>This is members page</h3>
+    <Link href="/">Home</Link>
+    </div>
+  )
+}
